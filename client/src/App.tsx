583c348750409cb437efd15c3d0c3b80999fdb48
Martin O'Donnell <modo1997@hotmail.co.uk>
2022-10-14 19:31:39 -0700

    setup way to show cards of stations within a certain distance

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,80 @@
-import React, { useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-interface IUser {
-  users: string[] | undefined;
+interface IStation {
+  lon: number;
+  legacy_id: number;
+  station_id: string;
+  lat: number;
+  capacity: number;
+  station_type: string;
+  region_id: number;
+  has_kiosk: boolean;
+  eightd_station_services: string[];
+  name: String;
+  rental_methods: string[];
+  rental_uris: {};
+  short_name: string;
+  electric_bike_surcharge_waiver: boolean;
 }
 
 function App() {
-  const [backendText, setBackendText] = useState<IUser>({ users: undefined });
+  const oneKmToGeocoding = 110.574;
+  const currentLocation = { lat: 37.78392060267635, lon: -122.43219948406649 };
+
+  const [distance, setDistance] = useState<number>(1);
+  const [stations, setStations] = useState<IStation[] | undefined>();
 
   useEffect(() => {
-    fetch("/api")
+    fetch("https://gbfs.baywheels.com/gbfs/fr/station_information.json")
       .then((response) => response.json())
-      .then((text) => {
-        console.log(text);
-        setBackendText(text);
+      .then((data) => {
+        setStations(data.data.stations.filter(withinXKM));
       });
-  }, []);
+  }, [distance]);
+
+  const distanceCalculator = () => distance / oneKmToGeocoding;
+
+  const withinXKM = ({ lat, lon }: IStation) => {
+    return (
+      currentLocation.lon - distanceCalculator() <= lon &&
+      lon <= currentLocation.lon + distanceCalculator() &&
+      currentLocation.lat - distanceCalculator() <= lat &&
+      lat <= currentLocation.lat + distanceCalculator()
+    );
+  };
 
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
 
       <div className="flex flex-col">
-        {backendText.users !== undefined ? (
-          backendText["users"].map((user, key) => <div key={key}>{user}</div>)
-        ) : (
+        {stations === undefined ? (
           <p>Loading</p>
+        ) : (
+          <div>
+            <input
+              value={distance}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setDistance(parseFloat(e.target.value))
+              }
+              type="number"
+            />
+
+            <h1>Stations: {stations.length}</h1>
+            {stations.map((station, key) => (
+              <div
+                key={key}
+                className="max-w-sm rounded overflow-hidden shadow-lg my-5"
+              >
+                <p>lat: {station.lat}</p>
+                <p>lon: {station.lon}</p>
+                {/* <p>capacity {station.capacity}</p>
+                <p>capacity {station.capacity}</p> */}
+              </div>
+            ))}
+          </div>
         )}
       </div>
     </div>
