f4c15771e9641f0e39bca5e944787edf71eeb476
Martin O'Donnell <modo1997@hotmail.co.uk>
2022-10-14 19:59:23 -0700

    setup station status ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,21 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Station from "./components/station";
 
 interface IStation {
   lon: number;
-  legacy_id: number;
-  station_id: string;
   lat: number;
   capacity: number;
-  station_type: string;
-  region_id: number;
-  has_kiosk: boolean;
-  eightd_station_services: string[];
-  name: String;
-  rental_methods: string[];
-  rental_uris: {};
-  short_name: string;
-  electric_bike_surcharge_waiver: boolean;
+  station_id: string;
+  // station_type: string;
+  // region_id: number;
+  // has_kiosk: boolean;
+  // eightd_station_services: string[];
+  // name: String;
+  // rental_methods: string[];
+  // rental_uris: {};
+  // short_name: string;
+  // electric_bike_surcharge_waiver: boolean;
 }
 
 function App() {
@@ -47,32 +46,24 @@ function App() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
       <div className="flex flex-col">
         {stations === undefined ? (
           <p>Loading</p>
         ) : (
           <div>
-            <input
-              value={distance}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                setDistance(parseFloat(e.target.value))
-              }
-              type="number"
-            />
-
+            <label>
+              Distance:
+              <input
+                value={distance}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setDistance(parseFloat(e.target.value))
+                }
+                type="number"
+              />
+            </label>
             <h1>Stations: {stations.length}</h1>
-            {stations.map((station, key) => (
-              <div
-                key={key}
-                className="max-w-sm rounded overflow-hidden shadow-lg my-5"
-              >
-                <p>lat: {station.lat}</p>
-                <p>lon: {station.lon}</p>
-                {/* <p>capacity {station.capacity}</p>
-                <p>capacity {station.capacity}</p> */}
-              </div>
+            {stations.map((station) => (
+              <Station station={station} />
             ))}
           </div>
         )}
---
+++ client/src/components/station.tsx
@@ -0,0 +1,78 @@
+import React, { ChangeEvent, useEffect, useState } from "react";
+
+interface IStation {
+  lon: number;
+  lat: number;
+  capacity: number;
+  station_id: string;
+}
+interface IStationStatus {
+  is_installed: number;
+  is_renting: number;
+  is_returning: number;
+  num_bikes_available: number;
+  num_bikes_disabled: number;
+  num_docks_available: number;
+  num_docks_disabled: number;
+  num_ebikes_available: number;
+  num_scooters_available: number;
+  num_scooters_unavailable: number;
+  station_id: string;
+  station_status: string;
+}
+
+function Station({ station }: { station: IStation }) {
+  const [stationStatus, setStationStatus] = useState<
+    undefined | null | IStationStatus
+  >();
+
+  useEffect(() => {
+    fetch("https://gbfs.baywheels.com/gbfs/en/station_status.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const filteredStations = data.data.stations.filter(filterStations);
+        if (filteredStations !== undefined && filteredStations.length === 1) {
+          setStationStatus(filteredStations[0]);
+        } else {
+          setStationStatus(null);
+        }
+      });
+  }, []);
+
+  const statusClassName = () => {
+    if (stationStatus !== null && stationStatus !== undefined) {
+      if (stationStatus.station_status === "active") {
+        return "bg-green-200";
+      } else {
+        return "bg-red-200";
+      }
+    }
+  };
+
+  const filterStations = (stat: IStation) => {
+    return stat.station_id === station.station_id;
+  };
+
+  return (
+    <div
+      key={station.station_id}
+      className={`max-w-sm rounded overflow-hidden shadow-lg my-5 ${statusClassName()}`}
+    >
+      <p>capacity: {station.capacity}</p>
+      <p>lat: {station.lat}</p>
+      <p>lon: {station.lon}</p>
+      {stationStatus === undefined ? (
+        <p>Loading status</p>
+      ) : stationStatus === null ? (
+        <p>Not found</p>
+      ) : (
+        <div>
+          <p>E Bikes {stationStatus.num_ebikes_available}</p>
+          <p>Bikes {stationStatus.num_bikes_available}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Station;
